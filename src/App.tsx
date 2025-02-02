6f22dc4d1c7c26db10caa5748b07eb2db4bf5aaf
Shoham Buskila <Shoham.Buskila@amdocs.com>
2025-02-02 22:29:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React, { useContext } from 'react';
-import './App.scss';
+import React, { useContext, useState } from 'react';
 import { PokemonModalContext, PokemonModalContextType } from 'context/PokemonModalProvider';
 import PokemonsContainer from 'components/pokemonsContainer/PokemonsContainer';
 import TypesBar from 'components/typesBar/TypesBar';
 import Modal from 'components/modal/mainModal/Modal';
-
+import './App.scss';
+import { Games } from 'components/games/Games';
 
 function App() {
   const { type } = useContext(PokemonModalContext) as PokemonModalContextType;
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const navigateToGame = () => {
+    setCurrentPage('game');
+  };
+
 
   return (
     <>
-      <div className='wrapper'>
-        <h1 className='logo-pokemon'>The world of Pokémons</h1>
-        <TypesBar />
-        <button className='button-play' onClick={() => window.location.href = '/game'}><span>Let's play</span></button>
-        <PokemonsContainer type={type} />
-      </div>
+      {currentPage === 'home' ? (
+        <div className='wrapper'>
+          <h1 className='logo-pokemon'>The world of Pokémons</h1>
+          <TypesBar />
+          <button className='button-play' onClick={navigateToGame}><span>Let's play</span></button>
+          <PokemonsContainer type={type} />
+        </div>
+      ) : (
+        <Games />
+      )}
       <Modal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
