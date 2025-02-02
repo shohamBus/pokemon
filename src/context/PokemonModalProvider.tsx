b40adb2e8a5f51b4c210f626d11321511393deca
Shoham Buskila <Shoham.Buskila@amdocs.com>
2025-02-02 21:31:28 +0200

    Update PokemonModalProvider.tsx

--- src/context/PokemonModalProvider.tsx
+++ src/context/PokemonModalProvider.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 export interface PokemonModalContextType {
     closeModal: () => void;
-    currentPokemon: Pokemon | null ;
+    currentPokemon: Pokemon | null;
     openModal: (pokemon: Pokemon) => void;
     isModalOpen: boolean;
     toggleType: (type: string) => void;
@@ -18,20 +18,20 @@ export const PokemonModalContext = React.createContext<PokemonModalContextType |
 //     children: React.ReactNode;
 // }
 
-export const PokemonModalProvider: React.FC<{children: React.ReactNode}> = ({ children }) => {
+export const PokemonModalProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [modal, setModal] = useState<{ isOpen: boolean; pokemon: Pokemon | null }>({ isOpen: false, pokemon: null });
     const [type, setType] = useState<string>('ice');
     const closeModal = () => setModal((prev) => ({ ...prev, isOpen: false }));
     const currentPokemon = modal.pokemon;
     const openModal = (pokemon: Pokemon) => setModal({ isOpen: true, pokemon: pokemon || null });
     const isModalOpen = modal.isOpen;
-    const toggleType= (type:string) => setType(type);
-console.log('type',type);
+    const toggleType = (type: string) => setType(type);
+    console.log('type', type);
 
 
     return (
-        <PokemonModalContext.Provider value={{ closeModal, currentPokemon, openModal, isModalOpen,toggleType ,type }}>
-            { children }
+        <PokemonModalContext.Provider value={{ closeModal, currentPokemon, openModal, isModalOpen, toggleType, type }}>
+            {children}
         </PokemonModalContext.Provider>
     );
 };
