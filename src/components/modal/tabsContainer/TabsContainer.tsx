894a2ee9af169d03f5915fef67f2133881302f60
Shoham Buskila <Shoham.Buskila@amdocs.com>
2025-02-02 22:36:01 +0200

    Update TabsContainer.tsx

--- src/components/modal/tabsContainer/TabsContainer.tsx
+++ src/components/modal/tabsContainer/TabsContainer.tsx
@@ -1,6 +1,6 @@
-import  React ,{ Suspense, useState } from 'react';
+import React, { Suspense, useState } from 'react';
 
-import './TabsContainer.scss'
+import './tabsContainer.scss'
 import About from '../tabs/About';
 import Stats from '../tabs/Stats';
 import Evolution from '../tabs/Evolution';
@@ -21,19 +21,19 @@ const TabsContainer = () => {
                 {
                     tabs.map(({ id }, index) => (
                         <button
-                            key={ id }
-                            className={ index === currentTab ? 'active' : undefined }
-                            onClick={ () => setCurrentTab(index) }
+                            key={id}
+                            className={index === currentTab ? 'active' : undefined}
+                            onClick={() => setCurrentTab(index)}
                         >
-                            { id }
+                            {id}
                         </button>
                     ))
                 }
             </nav>
-            
+
             <div className='data-container'>
-                <Suspense fallback={ <Loader /> }>
-     { tabs[currentTab].component }
+                <Suspense fallback={<Loader />}>
+                    {tabs[currentTab].component}
                 </Suspense>
             </div>
         </>
