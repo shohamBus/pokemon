dcb7650d75d98c407be42b3f12991eeb02621efd
Shoham Buskila <Shoham.Buskila@amdocs.com>
2025-02-04 17:15:32 +0200

    fix width

--- src/components/games/nameToPicture/nameToPictureGame/NameToPictureGame.tsx
+++ src/components/games/nameToPicture/nameToPictureGame/NameToPictureGame.tsx
@@ -3,11 +3,11 @@ import './nameToPictureGame.scss';
 import { EMPTY_STRING } from 'utils/constants';
 import { Pokemon } from 'interface/pokemon';
 
-interface NameToPictureGameProps{
-  type:string
-  stage:number
-  pokemons:Pokemon[]
-  setStage:React.Dispatch<React.SetStateAction<number>>
+interface NameToPictureGameProps {
+  type: string
+  stage: number
+  pokemons: Pokemon[]
+  setStage: React.Dispatch<React.SetStateAction<number>>
 }
 
 interface Card {
@@ -16,8 +16,8 @@ interface Card {
   bg: string;
 }
 
-export const NameToPictureGame = (props:NameToPictureGameProps) => {
-  const {type, stage,setStage, pokemons }=props;
+export const NameToPictureGame = (props: NameToPictureGameProps) => {
+  const { type, stage, setStage, pokemons } = props;
 
   const pokemonsForTheGame = useMemo(() => {
     let numberOfPokemons;
@@ -29,8 +29,8 @@ export const NameToPictureGame = (props:NameToPictureGameProps) => {
       numberOfPokemons = 12;
     }
     return pokemons?.sort(() => Math.random() - 0.5).slice(0, numberOfPokemons);
-  }, [stage,type]);
-  
+  }, [stage, type]);
+
   const resSet = useMemo(() => new Set(), []);
   const maxAttempts = useMemo(() => {
     if (stage === 1) return 6;
@@ -63,7 +63,7 @@ export const NameToPictureGame = (props:NameToPictureGameProps) => {
         return { name: pokemon.name, img: pokemon.imgSrc, bg: pokemon.types[0].name };
       }) || []
     );
-    setAttempts([0,false])
+    setAttempts([0, false])
     resSet.clear();
   }, [pokemonsForTheGame, resSet]);
 
@@ -80,7 +80,7 @@ export const NameToPictureGame = (props:NameToPictureGameProps) => {
         setTimeout(() => {
           setShowTitle(false);
           setStage((prev: number) => prev + 1);
-        }, 2000); 
+        }, 2000);
       }
       setAttempts((prevAttempts) => {
         const newAttempts: [number, boolean] = [prevAttempts[0] + 1, true];
@@ -93,60 +93,60 @@ export const NameToPictureGame = (props:NameToPictureGameProps) => {
             setAttempts([0, false]);
             resSet.clear();
             setSelectedCards({ name: undefined, image: undefined });
-          }, 2000); 
+          }, 2000);
         }
         return newAttempts;
       });
     }
   }, [resSet, cards, setStage, pokemonsForTheGame, maxAttempts, cardsForName, selectedCards]);
 
-    const getCardStyle=(name: string, check?: string)=>{
-      const checkName=check || undefined;
-     return  `card ${type} pokeball-bg-card ${checkName  === name ? 'selected' : resSet.has(name) ? 'matched' : EMPTY_STRING}`
-    }
+  const getCardStyle = (name: string, check?: string) => {
+    const checkName = check || undefined;
+    return `card ${type} pokeball-bg-card ${checkName === name ? 'selected' : resSet.has(name) ? 'matched' : EMPTY_STRING}`
+  }
 
-    const renderTitels=()=>{
-      return (
-        <>
-          {showTitle && stage !== 3 && <div className="big-title-success">Well done! Let's progress to stage {stage + 1}</div>}
-          {showTitle && stage === 3 && 
-            <div className="big-title-success">
-              Well done! You Are the king of the {type} world, Try another world
-            </div>
-          }
-          {(showMaxAttemptsTitle && !showTitle) && <div className="big-title-failed">You failed, no worries. Let's try again!</div>}
-        </>
-      );
-    }
+  const renderTitels = () => {
+    return (
+      <>
+        {showTitle && stage !== 3 && <div className="big-title-success">Well done! Let's progress to stage {stage + 1}</div>}
+        {showTitle && stage === 3 &&
+          <div className="big-title-success">
+            Well done! You Are the king of the {type} world, Try another world
+          </div>
+        }
+        {(showMaxAttemptsTitle && !showTitle) && <div className="big-title-failed">You failed, no worries. Let's try again!</div>}
+      </>
+    );
+  }
 
   return (
     <div>
       {renderTitels()}
-    <div className='count-section'>
-    <div className='count-box'>
-      <div>Count</div>
-      <span className={`${(selectedCards.name===selectedCards.image&&selectedCards.image!==undefined)   ? 'animate' : EMPTY_STRING}`}>{resSet.size}/{cards.length}</span>
-    </div>
-    <div className='count-box'>
-      <div>Attempts</div>
-      <span className={`attempts ${attempts[1] ? 'animate' : EMPTY_STRING}`} onAnimationEnd={() => setAttempts([attempts[0], false])}>{attempts[0]}/{maxAttempts}</span>
-    </div>
-  </div>
-    <div className="name-page-section">
-      <div className='cards-section'>
-        {cardsForName?.map((card, index) => (
-          <div onClick={() =>(!selectedCards.name  && !resSet.has(card.name)) &&  setSelectedCards((prev)=>({...prev, name: card.name}))}  key={`name-${index}`} className={getCardStyle(card.name, selectedCards.name)}>
-            <span className={`text-card `}>{card.name}</span></div>
-        ))}
+      <div className='count-section'>
+        <div className='count-box'>
+          <div>Count</div>
+          <span className={`${(selectedCards.name === selectedCards.image && selectedCards.image !== undefined) ? 'animate' : EMPTY_STRING}`}>{resSet.size}/{cards.length}</span>
+        </div>
+        <div className='count-box'>
+          <div>Attempts</div>
+          <span className={`attempts ${attempts[1] ? 'animate' : EMPTY_STRING}`} onAnimationEnd={() => setAttempts([attempts[0], false])}>{attempts[0]}/{maxAttempts}</span>
+        </div>
       </div>
-      <div className='cards-section'>
-        {cardsForImage?.map((card, index) => (
-          <div onClick={() => (!selectedCards.image  && !resSet.has(card.name)) && setSelectedCards((prev)=>({...prev, image: card.name}))} key={`img-${index}`} className={getCardStyle(card.name,selectedCards.image)}>
-            <img src={card.img} alt={card.name} className={`image-card `} />
-          </div>
-        ))}
+      <div className="name-page-section">
+        <div className='cards-section'>
+          {cardsForName?.map((card, index) => (
+            <div onClick={() => (!selectedCards.name && !resSet.has(card.name)) && setSelectedCards((prev) => ({ ...prev, name: card.name }))} key={`name-${index}`} className={getCardStyle(card.name, selectedCards.name)}>
+              <span className={`text-card `}>{card.name}</span></div>
+          ))}
+        </div>
+        <div className='cards-section'>
+          {cardsForImage?.map((card, index) => (
+            <div onClick={() => (!selectedCards.image && !resSet.has(card.name)) && setSelectedCards((prev) => ({ ...prev, image: card.name }))} key={`img-${index}`} className={getCardStyle(card.name, selectedCards.image)}>
+              <img src={card.img} alt={card.name} className={`image-card `} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   )
 }
